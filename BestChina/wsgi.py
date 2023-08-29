83875aaffbf145e6228737f0daae8c5d219c93fd
HosseinSayyedmousavi <hossein.sayyedmousavi@gmail.com>
2023-08-29 16:25:05 +0330

    debug

--- BestChina/wsgi.py
+++ BestChina/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BestChina project.
+WSGI config for Bestchina project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BestChina.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bestchina.settings')
 
 application = get_wsgi_application()
