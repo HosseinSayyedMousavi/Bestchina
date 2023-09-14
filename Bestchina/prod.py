0461a9f2e9850837f7c4b80263d0fbac2c38d094
HosseinSayyedmousavi <hossein.sayyedmousavi@gmail.com>
2023-09-14 12:03:35 +0330

    STATICFIELS_DIRS

--- Bestchina/prod.py
+++ Bestchina/prod.py
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/home/bestchin/public_html/static'
+STATICFIELS_DIRS = [
+BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
