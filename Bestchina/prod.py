f7b2e65da87ae407ad96cc6f1e3e682d3c30ca4c
hossein.sayyedmousavi <hossein.sayyedmousavi@gmail.com>
2023-09-18 09:59:17 +0330

    debug

--- Bestchina/prod.py
+++ Bestchina/prod.py
@@ -129,6 +129,5 @@ STATIC_ROOT = BASE_DIR / 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-IMPORT_ENDPOINT = "https://bestchina.ir/acc/wp-json/wpapi/v1/create-update-product"
-CATEGORY_ENDPOINT = "https://bestchina.ir/acc/wp-json/wpapi/v1/create-update-category"
-TIMEOUT = 360000
+IMPORT_ENDPOINT = "https://bestchina.ir/add/wp-json/wpapi/v1/create-update-product"
+CATEGORY_ENDPOINT = "https://bestchina.ir/add/wp-json/wpapi/v1/create-update-category"
