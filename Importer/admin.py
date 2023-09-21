20c8d53cb678338eab75688939934edd70d2019a
HosseinSayyedmousavi <hossein.sayyedmousavi@gmail.com>
2023-09-21 15:48:10 +0330

    debug

--- Importer/admin.py
+++ Importer/admin.py
@@ -23,7 +23,7 @@ class CategoryResource(ModelResource):
 
 class CategoryAdmin(ImportExportModelAdmin):
     list_display = ("Code","Name","FarsiName","ParentCode","Status")
-    readonly_fields= ("Code","Name","FarsiName","ParentCode","Status","errors","number_of_items","ItemList")
+    readonly_fields= ("Code","Name","FarsiName","ParentCode","Status","errors","number_of_items")
     search_fields = ("Name","Code","FarsiName")
 admin.site.register(Category,CategoryAdmin)
 
