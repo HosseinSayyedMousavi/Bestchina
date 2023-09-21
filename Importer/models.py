981558a861516f9ef2f82e3e94f80377953dcc81
HosseinSayyedmousavi <hossein.sayyedmousavi@gmail.com>
2023-09-21 15:41:27 +0330

    debug

--- Importer/models.py
+++ Importer/models.py
@@ -161,7 +161,7 @@ class Model_Black_List(models.Model):
 
 
 def Import_Job(importer):
-    try:
+    # try:
 
         AuthorizationToken = get_AuthorizationToken()
         if importer.category_prepared_Items == 0:
@@ -244,10 +244,10 @@ def Import_Job(importer):
                 importer.start_job=False
                 importer.save()
 
-    except Exception as e:
-        importer = Importer.objects.get(id=importer.id)
-        importer.status = "Stopped"
-        importer.errors = json.dumps(e.args)
-        importer.start_job=False
-        importer.save()
+    # except Exception as e:
+    #     importer = Importer.objects.get(id=importer.id)
+    #     importer.status = "Stopped"
+    #     importer.errors = json.dumps(e.args)
+    #     importer.start_job=False
+    #     importer.save()
 
