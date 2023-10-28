7ee34fbcac1971f42a396cb0174d2d1f10b66785
hossein.sayyedmousavi <hossein.sayyedmousavi@gmail.com>
2023-10-28 15:29:28 +0330

    debug

--- Bestchina/prod.py
+++ Bestchina/prod.py
@@ -76,26 +76,26 @@ WSGI_APPLICATION = 'Bestchina.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'accbestchina_db',
-#         'USER': 'accbestchina_user',
-#         'PASSWORD': 'R5sMknTvFQBS',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.mysql',
-		'NAME': 'accbestchina_db',
-		'USER': 'accbestchina_user',
-		'PASSWORD': 'R5sMknTvFQBS',
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'accbestchina_db',
+        'USER': 'accbestchina_user',
+        'PASSWORD': 'R5sMknTvFQBS',
         'HOST': 'localhost',
-        'PORT': '3306',
-	}
+        'PORT': '5432',
+    }
 }
+# DATABASES = {
+# 	'default': {
+# 		'ENGINE': 'django.db.backends.mysql',
+# 		'NAME': 'accbestchina_db',
+# 		'USER': 'accbestchina_user',
+# 		'PASSWORD': 'R5sMknTvFQBS',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+# 	}
+# }
 
 
 # Password validation
