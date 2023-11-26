d8106b812c742ea84de846aee30edb21b01e25af
hossein.sayyedmousavi <hossein.sayyedmousavi@gmail.com>
2023-11-26 15:26:52 +0330

    debug

--- Importer/admin.py
+++ Importer/admin.py
@@ -31,7 +31,7 @@ class ImporterAdmin(admin.ModelAdmin):
     list_display = ("category","status","is_periodic","Progress_percentage","Progress_bar")
     list_display_links = list_display
     exclude = ["start_job"]
-    readonly_fields = ("is_periodic","period_length","category","category.number_of_items","Progress_percentage","Progress_bar","period_number","Number_of_products","Number_of_checked_products","errors","updated_at","created_at","current_Item","operation","formula")
+    readonly_fields = ("is_periodic","period_length","category","category__number_of_items","Progress_percentage","Progress_bar","period_number","Number_of_products","Number_of_checked_products","errors","updated_at","created_at","current_Item","operation","formula")
     search_fields = ("category.Name","category.Code")
 admin.site.register(Importer,ImporterAdmin)
 
